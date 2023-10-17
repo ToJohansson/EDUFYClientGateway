d7c6eabd05c0e39e3540fa00ba6fdca90f8ae7d6
Tobias Johansson <tobiasp91@hotmail.com>
2023-10-17 10:55:40 +0200

    Initial commit

---
+++ src/main/java/tobiasjohansson/m3clientgateway/M3ClientGatewayApplication.java
@@ -0,0 +1,13 @@
+package tobiasjohansson.m3clientgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3ClientGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3ClientGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tobiasjohansson/m3clientgateway/M3ClientGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tobiasjohansson.m3clientgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3ClientGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
